6cc731511fc3d842b0079094d45f02ec937df896
Fritz Batroni <batroni@adobe.com>
2020-08-06 09:31:23 -0400

    test for creating new org

--- test/routes.test.js
+++ test/routes.test.js
@@ -3,21 +3,19 @@ require('dotenv').config();
 const request = require('supertest');
 const app = require('../src/index.js');
 describe('User API', () => {
-  // it('should create a new organization', async () => {
-  //   const res = await request(app)
-  //     .post('/api/v1/organization/new')
-  //     .send({
-  //       orgname: 'Test org103',
-  //       startDate: new Date(),
-  //       numberOfEmployees: 5,
-  //       isPublic: true,
-  //     })
-  //   expect(res.statusCode).toEqual(201);
-  // }),
-  it('should find an org based on orgname criteria', async () => {
-    const res = await request(app).post('/api/v1/organization/search').send({
+  it('should create a new organization', async () => {
+    const res = await request(app).post('/api/v1/organization/new').send({
       orgname: 'Test org103',
+      startDate: new Date(),
+      numberOfEmployees: 5,
+      isPublic: true,
     });
-    expect(res.statusCode).toEqual(200);
-  });
+    expect(res.statusCode).toEqual(201);
+  }),
+    it('should find an org based on orgname criteria', async () => {
+      const res = await request(app).post('/api/v1/organization/search').send({
+        orgname: 'Test org103',
+      });
+      expect(res.statusCode).toEqual(200);
+    });
 });
